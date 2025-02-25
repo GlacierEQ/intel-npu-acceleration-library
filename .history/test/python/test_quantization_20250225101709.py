0082f189c8a248ca07bb47aa6b31c605b72224a6
GlacierEQ <GLACIER.EQUILIBRIUM@GMAIL.COM>
2025-02-25 10:17:43 -1000

    Create test_quantization_20250225101709.py

---
+++ .history/test/python/test_quantization_20250225101709.py
@@ -0,0 +1,150 @@
+#
+# Copyright © 2024 Intel Corporation
+# SPDX-License-Identifier: Apache 2.0
+#
+
+from sklearn.metrics import r2_score
+import intel_npu_acceleration_library.quantization
+import intel_npu_acceleration_library
+import numpy as np
+from intel_npu_acceleration_library.compiler import CompilerConfig
+import pytest
+import torch
+from intel_npu_acceleration_library.quantization import quantize_tensor
+from intel_npu_acceleration_library.dtypes import bfloat16
+
+
+def test_quantization_with_bfloat16():
+    """Test quantization of a tensor to BFloat16."""
+    input_tensor = torch.tensor([1.0, 2.0, 3.0], dtype=torch.float32)
+    quantized_tensor, scale_tensor = quantize_tensor(input_tensor)
+
+    assert quantized_tensor.dtype == torch.int8
+    assert quantized_tensor.shape == input_tensor.shape
+    assert torch.allclose(quantized_tensor.float(), input_tensor, rtol=1e-2, atol=1e-2)
+
+
+class NN(torch.nn.Module):
+    def __init__(self, inC, outC):
+        super().__init__()
+        self.l1 = torch.nn.Linear(inC, outC, bias=False)
+
+    def forward(self, x):
+        return self.l1(x)
+
+
+@pytest.mark.parametrize("batch", [16, 128])
+@pytest.mark.parametrize("inC", [256, 512])
+@pytest.mark.parametrize("outC", [256, 512])
+def test_explicit_quantization(batch, inC, outC):
+    module = intel_npu_acceleration_library.backend.NNFactory()
+    assert module
+
+    input = module.parameter((batch, inC))
+    assert input
+
+    output = module.linear(input, outC, inC)
+    assert output
+
+    module.compile()
+
+    X = np.random.random((batch, inC)).astype(np.float16)
+    W = np.random.randint(-127, 127, (outC, inC)).astype(np.int8)
+    S = np.random.random((outC, 1)).astype(np.float32)
+
+    w_float = W.astype(np.float16) * S
+    y_ref = np.matmul(X, w_float.T)
+
+    y = module.run(X, (W, S), op_id="0000")
+
+    assert 1 - r2_score(y_ref, y) < 0.01
+
+
+@pytest.mark.parametrize("batch", [16, 128])
+@pytest.mark.parametrize("inC", [256, 512])
+@pytest.mark.parametrize("outC", [256, 512])
+def test_i8_quantization(batch, inC, outC):
+    module = intel_npu_acceleration_library.backend.NNFactory()
+    assert module
+
+    input = module.parameter((batch, inC))
+    assert input
+
+    output = module.linear(input, outC, inC, False, wt_dtype=np.int8)
+    assert output
+
+    module.compile()
+
+    X = np.random.random((batch, inC)).astype(np.float16)
+    W = np.random.randint(-127, 127, (outC, inC)).astype(np.int8)
+    S = np.random.random((outC, 1)).astype(np.float16)
+
+    w_float = W.astype(np.float16) * S
+    y_ref = np.matmul(X, w_float.T)
+
+    y = module.run(X, (W, S * np.sqrt(inC)), op_id="0000")
+
+    assert 1 - r2_score(y_ref, y) < 0.01
+
+
+@pytest.mark.parametrize("batch", [16, 128])
+@pytest.mark.parametrize("inC", [256, 512])
+@pytest.mark.parametrize("outC", [256, 512])
+def test_compiled_quantized(batch, inC, outC):
+
+    intel_npu_acceleration_library.backend.clear_cache()
+
+    torch.manual_seed(0)
+    X = torch.rand((batch, inC), dtype=torch.float16) - 0.5
+    # X = np.random.random((batch, inC)).astype(np.float16)
+
+    model = NN(inC, outC)
+    y_ref = model(X.to(torch.float32)).detach()
+
+    compiler_conf = CompilerConfig(dtype=torch.int8)
+    compiled_model = intel_npu_acceleration_library.compile(model, compiler_conf)
+    assert compiled_model
+
+    y1 = compiled_model(X).detach()
+
+    assert 1 - r2_score(y_ref.numpy(), y1.numpy()) < 0.01
+
+
+@pytest.mark.parametrize("batch", [16, 128])
+@pytest.mark.parametrize("inC", [256, 512])
+@pytest.mark.parametrize("outC", [256, 512])
+def test_i4_quantization(batch, inC, outC):
+
+    module = intel_npu_acceleration_library.backend.NNFactory()
+    assert module
+
+    input = module.parameter((batch, inC))
+    assert input
+    # u8 represents packed i4 dtypes
+    output = module.linear(input, outC, inC, False, wt_dtype=np.uint8)
+    assert output
+
+    module.compile()
+
+    X = np.random.random((batch, inC)).astype(np.float16)
+    S = np.random.random((outC, 1)).astype(np.float16)
+    W = np.random.randint(-8, 7, (outC, inC)).astype(np.int8)
+
+    w_float = W.astype(np.float16) * S
+    y_ref = np.matmul(X, w_float.T)
+
+    # Compress the weights for int4
+    W_npu = intel_npu_acceleration_library.quantization.compress_to_i4(
+        torch.from_numpy(W)
+    ).numpy()
+
+    y = module.run(X, (W_npu, S * np.sqrt(inC)), op_id="0000")
+
+    # assert y has no NaN
+    assert not np.isnan(y).any()
+
+    # assert y has no Inf
+    assert not np.isinf(y).any()
+
+    # Check for correctness vs reference
+    assert 1 - r2_score(y_ref.flatten(), y.flatten()) < 0.01
